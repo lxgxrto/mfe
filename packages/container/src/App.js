fbc22229d6cd21f00b65fbf4903e7c438ffa933b
juan.flores@inegi.org.mx <juan.flores@inegi.org.mx>
2021-03-11 16:15:07 -0600

    Another testing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MarketingApp from '../components/MarketingApp';
 
 export default () => {
-    console.log('testing')
+    console.log('another testing')
     return (
         <div>
             <h1>Hi there!</h1>
